bd4b67c316fc4714cf2a5d9b4467e84ef61fb2e4
Raagul Vignesh R <94353294+RaagulVigneshR@users.noreply.github.com>
2022-11-26 11:26:54 +0530

    Create client.c

---
+++ FULL DUPLEX CHAT USING TCP-IP/client.c
@@ -0,0 +1,109 @@
+#include "stdio.h"
+
+#include "stdlib.h"
+
+#include "string.h"
+
+//headers for socket and related functions
+
+#include <sys/types.h>
+
+#include <sys/socket.h>
+
+//for including structures which will store information needed
+
+#include <netinet/in.h>
+
+#include <unistd.h>
+
+//for gethostbyname
+
+#include "netdb.h"
+
+#include "arpa/inet.h"
+
+int main()
+
+{
+
+int socketDescriptor;
+
+struct sockaddr_inserverAddress;
+
+char sendBuffer[8000],recvBuffer[8000];
+
+pid_tcpid;
+
+bzero(&serverAddress,sizeof(serverAddress));
+
+serverAddress.sin_family=AF_INET;
+
+serverAddress.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+serverAddress.sin_port=htons(9652);
+
+/*Creating a socket, assigning IP address and port number for that socket*/
+
+socketDescriptor=socket(AF_INET,SOCK_STREAM,0);
+
+/*Connect establishes connection with the server using server IP address*/
+
+connect(socketDescriptor,(struct sockaddr*)&serverAddress,sizeof(serverAddress));
+
+/*Fork is used to create a new process*/
+
+cpid=fork();
+
+if(cpid==0)
+
+{
+
+while(1)
+
+{
+
+bzero(&sendBuffer,sizeof(sendBuffer));
+
+printf("\nType a message here ... ");
+
+/*This function is used to read from server*/
+
+fgets(sendBuffer,80000,stdin);
+
+ 
+
+/*Send the message to server*/
+
+send(socketDescriptor,sendBuffer,strlen(sendBuffer)+1,0);
+
+printf("\nMessage sent !\n");
+
+ 
+
+}
+
+}
+
+else
+
+{
+
+while(1)
+
+{
+
+bzero(&recvBuffer,sizeof(recvBuffer));
+
+/*Receive the message from server*/
+
+recv(socketDescriptor,recvBuffer,sizeof(recvBuffer),0);
+
+printf("\nSERVER : %s\n",recvBuffer);
+
+}
+
+}
+
+return 0;
+
+}
